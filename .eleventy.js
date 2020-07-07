e23bf6dc3a2c6ec93b44f604bcaf19b1c171c89d
syun <syun.divetolife@gmail.com>
2020-07-07 11:59:52 +0900

    静的サイトジェネレーターEleventy 第6回 提供される日時データと表示方法

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
-module.exports = {
-  dir: {
-    input: "src"
+// module.exports = {
+//   dir: {
+//     input: "src"
+//   }
+// };
+
+const dayjs = require('dayjs');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return dayjs(dateObj).format('YYYY年M月D日');
+  });
+
+  return {
+    dir: {
+      input: "src"
+    }
   }
 };
