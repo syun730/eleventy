615139ccc915a7d9816d3b9818974345cfa5443a
syun <syun.divetolife@gmail.com>
2020-07-07 12:33:13 +0900

    静的サイトジェネレーターEleventy 第7回 ページ一覧を作成するコレクションデータ

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,11 @@ module.exports = function(eleventyConfig) {
     return dayjs(dateObj).format('YYYY年M月D日');
   });
 
+  eleventyConfig.addFilter("year", (collection, year) => {
+    year = year || new Date().getFullYear(); // 年が指定されない場合は今年を表示する
+    return collection.filter(item => item.date.getFullYear() === year);
+  });
+
   return {
     dir: {
       input: "src"
