3e018c6cd7d8ae3e584a97bb1fe9f23f1b931ce5
syun <syun.divetolife@gmail.com>
2020-07-07 12:51:15 +0900

    静的サイトジェネレーターEleventy 第8回 EleventyでCSSを扱う

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,9 @@
 // };
 
 const dayjs = require('dayjs');
+const CleanCSS = require("clean-css");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("readableDate", dateObj => {
@@ -14,6 +17,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("year", (collection, year) => {
     year = year || new Date().getFullYear(); // 年が指定されない場合は今年を表示する
     return collection.filter(item => item.date.getFullYear() === year);
+  });
+
+  // assetsディレクトリはそのまま出力先にコピー
+  eleventyConfig.addPassthroughCopy("assets");
+
+  // css minify
+  eleventyConfig.addFilter("cssmin", code => {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  // postcss Autoprefixer
+  eleventyConfig.addFilter("postcss", function(code) {
+    return postcss([autoprefixer]).process(code).css;
   });
 
   return {
